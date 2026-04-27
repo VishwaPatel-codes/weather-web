3c17a94bd5ff1242642c580d8d0f3697ece71d64
Vishwa Patel <pvishwa2917@gmail.com>
2026-04-27 15:08:00 +0530

    weather api

---
+++ script.js
@@ -0,0 +1,20 @@
+const apiKey = '136e0b3d22cfba7b5a2951f96d8ccf04';
+
+const button = document.querySelector('button');
+const Input = document.querySelector('input');
+
+button.addEventListener("click", () => {
+    const city = Input.value;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector('.weather-info h3').textContent = data.name;
+            
+            document.querySelector('.weather-info p:nth-child(2)').textContent = 
+                'Temperaturre: ' + Math.round(data.main.temp) + '°C';
+
+            document.querySelector('.weather-info p:nth-child(3)').textContent = 
+                'condition: ' + data.weather[0].main;
+        });
+});
